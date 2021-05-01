6e394f44e4317c579fdf826707ecd2c0c47dfba5
Oliver Xu <olxu@hotmail.com>
2021-05-02 00:30:19 +1000

    basic navbarPage implemented

--- app.R
+++ app.R
@@ -15,20 +15,32 @@ appCSS <-
     ".mandatory_star { color: red; }"
 
 shinyApp(
-    ui <- fluidPage(
-        shinyjs::useShinyjs(),
-        shinyjs::inlineCSS(appCSS),
-        titlePanel("COVID-19 Risk Calculator"),
-        div(
-            id="form",
-            
-            selectInput("gender", "Gender", c("", "Male", "Female")),
-            textInput("age", "Age", ""), 
-            actionButton("submit", "Submit", class="btn-primary")
+    ui <- navbarPage(
+        title = "COVID-19 Risk Calculator",
+        id = "navbar",
+        tabPanel(
+            title = "Patient Form",
+            fluidPage(
+                shinyjs::useShinyjs(),
+                shinyjs::inlineCSS(appCSS),
+                titlePanel(""),
+                div(
+                    id="form",
+                    
+                    selectInput("gender", "Gender", c("", "Male", "Female")),
+                    textInput("age", "Age", "")
+                )
+            ),
+            actionButton("button", "Submit")
+        ),
+        tabPanel(
+            title = "Results",
+            value = "result",
+            verbatimTextOutput("results")
         )
     ),
     
-    server <- function(input, output) {
+    server <- function(input, output, session) {
         observe({
             mandatoryFilled <- 
                 vapply(fieldsMandatory,
@@ -40,7 +52,15 @@ shinyApp(
             
             shinyjs::toggleState(id="submit", condition=mandatoryFilled)
         })
-        # TODO: classification logic
+        
+        observeEvent(input$button, {
+            updateNavbarPage(session, "navbar",
+                             selected="result")
+        })
+        
+        output$results <- renderPrint({
+            p(input$gender, input$age)
+        })
     }
 )
 
